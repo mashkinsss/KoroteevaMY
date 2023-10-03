2d969ddf35b8ae68615f3c95714b87478d3b5e4c
Maria Koroteeva <146769718+mashkinsss@users.noreply.github.com>
2023-10-03 15:31:58 +0300

    Update Занятие2.py

--- Занятие2.py
+++ Занятие2.py
@@ -9,10 +9,10 @@ x = -4.5
 y = 0.75*10**(-4)
 z = -0.845*10**2
 s = (((9+(x-y)**2)**(1/3))/(x**2+y**2 +2))-math.exp(abs(x -y))*(math.tan(z))**3
-print(s)
+print(s) #2
 
 x = 0.1722
 y = 6.33
 z = 3.25*10**(-4)
 s = 5 * math.atan(x) - 1/4*math.acos(x)*(x+3*abs(x - y)+x**2)/(abs(x-y)*z+x**2)
-print(s)
+print(s) #7
