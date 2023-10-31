d36dc85ff9ea9a4417ae0da547682651f1e1ab4c
mashkinsss <koroteewa.masha2015@yandex.ru>
2023-10-31 21:47:59 +0300

    1 занятие
    1 занятие

---
+++ Занятие 5/1 задание.py
@@ -0,0 +1,13 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+def ppppp(n):
+    i = 1
+    while i**2 <= n:
+        print(i**2)
+        i += 1
+n=int(input())
+r= ppppp(n)
+print(r)
+SyntaxError: invalid syntax
+>>> 
