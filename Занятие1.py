16e0e65cb3811f617b03ba79e517d45d82c337e7
Maria Koroteeva <146769718+mashkinsss@users.noreply.github.com>
2023-11-23 02:56:27 +0300

    Занятие1.py

--- Занятие1.py
+++ Занятие1.py
@@ -1,7 +1,10 @@
+# -- coding: utf-8 --
 print('Курс Основы программирования начался') #1
 
+# -- coding: utf-8 --
 print(16823*12302%3092) #2
 
+# -- coding: utf-8 --
 age = int(input('возраст абитуриента')) #3
 name = int(input('имя абитуриента'))
 if 0 < age < 75 and name != 'Иван':
@@ -13,10 +16,12 @@ if 0 < age < 75 and name != 'Иван':
 else:
     print('не соответствует критериям')
 
+# -- coding: utf-8 --
 print("Напишите кол-во секунд") #4
 sec = int(input())
 print(sec//86400, (sec%86400)//3600, ((sec%86400)%3600)//60,(((sec%86400)%3600)%60))
 
+# -- coding: utf-8 --
 n = int(input()) #5
 print(n + n**2 + n**3 + n**4 + n**5)
 x = input()#6
@@ -25,6 +30,7 @@ print(x,y)
 x,y = y,x
 print(x,y)
 
+# -- coding: utf-8 --
 print("Напишите значения a и b") #6
 a=int(input())
 b=int(input())
@@ -33,6 +39,7 @@ b=a
 a=c
 print("a-",a," b-",b)
 
+# -- coding: utf-8 --
 print("Дайте значение Num:")#7
 num=int(input())
 if num%2 == 0:
