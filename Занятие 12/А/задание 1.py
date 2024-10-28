1b728e6b6e53910c0492e3a19c8cd9818acb6bd1
Maria Koroteeva <146769718+mashkinsss@users.noreply.github.com>
2024-10-28 19:14:00 +0300

    Delete Занятие 12 directory

--- Занятие 12/А/задание 1.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-def Solution(N):
-    global X
-    if N == 0:
-        return 1
-    return (X**N) // (Solution(N-1) * N)
-X = 10
-print(Solution(15))
--- Занятие 12/А/задание 5.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-def GetSumA(A):
-    if A // 10 == 0:
-        return A
-    else:
-        print(A % 10)
-        return GetSumA(A // 10)
-
-print(GetSumA(123))
